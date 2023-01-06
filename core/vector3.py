0cd956698086474122e06038d35e31d527177c56
DungyBug <ruslanbez2006@gmail.com>
2023-01-06 19:12:04 +0300

    add cameras

--- core/vector3.py
+++ core/vector3.py
@@ -1,14 +1,14 @@
-from math import sqrt
+from math import hypot
 
 
 class Vector3:
-    def __init__(self, x: float, y: float, z: float):
+    def __init__(self, x: float, y: float = None, z: float = None):
         self.x = x
-        self.y = y
-        self.z = z
+        self.y = y or self.x
+        self.z = z or self.y
 
     def length(self):
-        return sqrt(self.x * self.x + self.y * self.y + self.z * self.z)
+        return hypot(self.x, self.y, self.z)
 
     def __add__(self, another):
         return Vector3(another.x + self.x, another.y + self.y, another.z + self.z)
@@ -16,7 +16,7 @@ class Vector3:
     def __sub__(self, another):
         return Vector3(self.x - another.x, self.y - another.y, self.z - another.z)
 
-    def __div__(self, another):
+    def __truediv__(self, another):
         return Vector3(self.x / another.x, self.y / another.y, self.z / another.z)
 
     def __mul__(self, another):
@@ -24,3 +24,19 @@ class Vector3:
 
     def __eq__(self, another):
         return self.x == another.x and self.y == another.y and self.z == another.z
+
+    def normalize(self):
+        length = self.length()
+
+        self.x /= length
+        self.y /= length
+        self.z /= length
+
+    @staticmethod
+    def normalized(vec):
+        length = vec.length()
+
+        if length == 0:
+            return Vector3(0, 0, 0)
+
+        return vec / Vector3(length)
--- main.py
+++ main.py
@@ -8,21 +8,30 @@ import pythree
 from core.world import World
 from core.engine import Engine
 from core.registry import Registry
+from client.camera import Camera
 from client.renderers.opengl.opengl_renderer import OpenGLRenderer
+from OpenGL.GL import *
+import glm
 
 keys = [0 for _ in range(255)]
 
+clock = pygame.time.Clock()
+
 pygame.init()
+pygame.font.init()
 pygame.display.gl_set_attribute(pygame.GL_CONTEXT_MAJOR_VERSION, 3)
 pygame.display.gl_set_attribute(pygame.GL_CONTEXT_MINOR_VERSION, 3)
 pygame.display.gl_set_attribute(
     pygame.GL_CONTEXT_PROFILE_MASK, pygame.GL_CONTEXT_PROFILE_CORE)
-pygame.display.set_mode((640, 480), flags=pygame.OPENGL | pygame.DOUBLEBUF)
+pygame.display.set_mode(
+    (1920, 1080), flags=pygame.OPENGL | pygame.DOUBLEBUF)
 
 world = World(Registry())
 engine = Engine(world)
 
-renderer = OpenGLRenderer(640, 480)
+camera = Camera()
+
+renderer = OpenGLRenderer(1920, 1080, Camera())
 
 engine.add_module(renderer)
 
@@ -32,29 +41,68 @@ triangle = pythree.Geometry([
     0.0, 0.0,   0.5,
     1.0, 0.0,  -0.5
 ])
+triangle2 = pythree.Geometry([
+    -1.0, 0.0, -0.5,
+    0.0, 0.0,   0.5,
+    1.0, 0.0,  -0.5
+])
 
 entity = ViewableEntity("ent_test", Vector3(
-    0.0, -4.0, 5.0), Mesh(triangle, engine))
+    0.0, -4.0, 0.0), Mesh(triangle, engine), rotation=Vector3(0, 0.00, 0))
 
+entity2 = ViewableEntity("ent_test2", Vector3(
+    0.0, 0.0, 0.0), Mesh(triangle2, engine), scale=Vector3(10, 10, 10), rotation=Vector3(0, 0.00, 0))
+
+world.add_entity(entity2)
 world.add_entity(entity)
 
+frame = 0
 
 while True:
     engine.tick()
 
+    entity.position.y = sin(frame / 60 * 2 * pi) + 1.0
+    entity.rotation.x += 120.0 / 60.0 * pi / 180.0
+    entity.rotation.y += 60.0 / 60.0 * pi / 180.0
+    entity.rotation.z += 30.0 / 60.0 * pi / 180.0
+
+    mouse_pos = pygame.mouse.get_pos()
+
+    if keys[pygame.key.key_code('q')]:
+        camera.look_at(entity.position)
+    else:
+        camera.rotation.y = mouse_pos[0] / 3840 * pi * 2 - pi
+        camera.rotation.x = mouse_pos[1] / 2160 * pi * 2 - pi
+
+    forward = glm.vec3(0.0, 0.0, 1.0)
+
+    forward = glm.rotate(forward, camera.rotation.x,
+                         glm.vec3(1.0, 0.0, 0.0))
+    forward = glm.rotate(forward, camera.rotation.y,
+                         glm.vec3(0.0, 1.0, 0.0))
+
+    left = glm.vec3(1.0, 0.0, 0.0)
+
+    left = glm.rotate(left, camera.rotation.y,
+                      glm.vec3(0.0, 1.0, 0.0))
+
     if keys[pygame.key.key_code('w')]:
-        entity.position.x -= sin(-entity.rotation.y) * 5.0 / 60.0
-        entity.position.z -= cos(-entity.rotation.y) * 5.0 / 60.0
+        camera.position.x += forward.x * 5.0 / 60.0
+        camera.position.y += forward.y * 5.0 / 60.0
+        camera.position.z += forward.z * 5.0 / 60.0
 
     if keys[pygame.key.key_code('s')]:
-        entity.position.x += sin(-entity.rotation.y) * 5.0 / 60.0
-        entity.position.z += cos(-entity.rotation.y) * 5.0 / 60.0
+        camera.position.x -= forward.x * 5.0 / 60.0
+        camera.position.y -= forward.y * 5.0 / 60.0
+        camera.position.z -= forward.z * 5.0 / 60.0
 
     if keys[pygame.key.key_code('a')]:
-        entity.rotation.y += 120.0 / 60.0 * pi / 180
+        camera.position.x -= left.x * 5.0 / 60.0
+        camera.position.z -= left.z * 5.0 / 60.0
 
     if keys[pygame.key.key_code('d')]:
-        entity.rotation.y -= 120.0 / 60.0 * pi / 180
+        camera.position.x += left.x * 5.0 / 60.0
+        camera.position.z += left.z * 5.0 / 60.0
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -66,4 +114,7 @@ while True:
             keys[event.key] = False
 
     pygame.display.flip()
-    pygame.time.wait(16)
+
+    clock.tick(60)
+
+    frame += 1
