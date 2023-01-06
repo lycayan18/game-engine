4ed93c95a1b42a6e465a39bf832b433cede73c11
DRAKUTONT <grudnezlov@gmail.com>
2023-01-06 22:22:44 +0300

    change something arguments

--- client/client_entity.py
+++ client/client_entity.py
@@ -6,7 +6,11 @@ from core.mesh import Mesh
 
 class ClientEntity(ViewableEntity):
     def __init__(self, entity: Entity, class_name: str, mesh: Mesh = None,
-                 rotation: Vector3 = Vector3(0, 0, 0), scale: Vector3 = Vector3(1, 1, 1)):
+                 rotation: Vector3 = None, scale: Vector3 = None):
+
+        rotation = rotation or Vector3(0, 0, 0)
+        scale = scale or Vector3(1, 1, 1)
+
         super().__init__(class_name, entity.position, mesh, rotation, scale)
         self.entity = entity
 
